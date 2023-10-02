930387000716eb5f243b7757728deb650ce39a23
code-daddy1 <100324675+code-daddy1@users.noreply.github.com>
2023-10-01 22:42:06 -0500

    Added a feature that branches the program into two different financial calculators

--- budgetCalculator.cpp
+++ budgetCalculator.cpp
@@ -57,176 +57,256 @@ int main() {
 
 	string save;
 	string invest;
+	string programChoice;
 
-
-	do {
-		cout << "What is the husband's income?\n";
-		cin >> husbandIncome;
-
-		if (husbandIncome >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
 
 
 	do {
-		cout << "What is the wife's income?\n";
-		cin >> wifeIncome;
+		cout << "Which program would you like to run? (Budget or Expense)\n";
+		cin >> programChoice;
 
-		if (wifeIncome >= 0) {
+		if (programChoice == "expense") {
+
+			do {
+				cout << "What is the husband's income?\n";
+				cin >> husbandIncome;
+
+				if (husbandIncome >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+
+			do {
+				cout << "What is the wife's income?\n";
+				cin >> wifeIncome;
+
+				if (wifeIncome >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			do {
+				cout << "How much was spent on gas?\n";
+				cin >> gasExpense;
+
+				if (gasExpense >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			do {
+				cout << "How much was spent on eating out?\n";
+				cin >> foodExpense;
+
+				if (foodExpense >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			do {
+				cout << "How much was spent on the mortgage?\n";
+				cin >> mortgageExpense;
+
+				if (mortgageExpense >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			do {
+				cout << "How much was spent on utilities?\n";
+				cin >> utilityExpense;
+
+				if (utilityExpense >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			do {
+				cout << "How much was spent on groceries?\n";
+				cin >> groceriesExpense;
+
+				if (groceriesExpense >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			do {
+				cout << "How much was spent on bills?\n";
+				cin >> billsExpense;
+
+				if (billsExpense >= 0) {
+					break;
+				}
+				else {
+					cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+				}
+			} while (true);
+
+			cin.ignore();
+
+			cout << "Do you put money in savings?\n";
+			getline(cin, save);
+
+			cout << "Do you put money aside for investing?\n";
+			getline(cin, invest);
+
+			cout << setprecision(2) << fixed;
+
+			totalIncome = husbandIncome + wifeIncome;
+
+			double gasPercent = (gasExpense / totalIncome) * 100;
+			double foodPercent = (foodExpense / totalIncome) * 100;
+			double mortgagePercent = (mortgageExpense / totalIncome) * 100;
+			double utilityPercent = (utilityExpense / totalIncome) * 100;
+			double groceriesPercent = (groceriesExpense / totalIncome) * 100;
+			double billsPercent = (billsExpense / totalIncome) * 100;
+
+			if (save == "yes" || save == "Yes" || save == "Y" || save == "y") {
+				cout << "How much did you set aside into savings?\n";
+				cin >> savings;
+			}
+
+			else if (save == "no" || save == "No" || save == "n" || save == "N") {
+				cout << endl;
+				savings = 0;
+			}
+
+			if (invest == "yes" || invest == "Yes" || invest == "Y" || invest == "y") {
+				cout << "How much was set aside for investments?\n";
+				cin >> investments;
+			}
+
+			else if (invest == "no" || invest == "No" || invest == "n" || invest == "N") {
+
+				cout << endl;
+				investments = 0;
+			}
+
+
+			totalIncome = calculateIncome(husbandIncome, wifeIncome);
+			totalExpense = calculateExpense(gasExpense, foodExpense, mortgageExpense, utilityExpense, groceriesExpense, billsExpense);
+			piggyBank = savings + investments;
+			piggyBankPercent = (piggyBank / totalIncome) * 100;
+			savingsPercent = (savings / totalIncome) * 100;
+			investmentPercent = (investments / totalIncome) * 100;
+			moneyRemaining = totalIncome - totalExpense - piggyBank;
+
+			cout << endl;
+
+			if (totalExpense > totalIncome) {
+				cout << "How the fuck are you going to spend more than you make???\n";
+			}
+
+			cout << endl;
+
+			cout << "Your total household income is $" << totalIncome << endl;
+			cout << "Your total expense is $" << totalExpense << endl;
+			cout << gasPercent << "% of the income was spent on gas." << endl;
+			cout << foodPercent << "% of the income was spent on eating out." << endl;
+			cout << mortgagePercent << "% of the income was spent on the mortgage." << endl;
+			cout << utilityPercent << "% of the income was spent on utilities." << endl;
+			cout << groceriesPercent << "% of the income was spent on groceries." << endl;
+			cout << billsPercent << "% of the income was spent on bills." << endl;
+			cout << endl;
+			cout << "Your total amount set aside for savings and investment is $" << piggyBank << endl;
+			cout << "This accounts for " << piggyBankPercent << "% of the total household income." << endl;
+			cout << "Your remaining cash available is $" << moneyRemaining;
+
+			cout << endl;
 			break;
+
+			
 		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
+
+		double savingsRate;
+		
+
+		if(programChoice == "budget" || programChoice == "Budget") {
+			
+			
+
+			do {
+				cout << "How much money does the husband make each month?" << endl;
+				cin >> husbandIncome;
+
+				if (husbandIncome >= 0) {
+					cout << endl;
+					break;
+				}
+				else {
+					cout << "Invalid input, please try entering a dollar amount.";
+				}
+			} while (true);
+
+			do {
+				cout << "How much money does the wife make each month?" << endl;
+				cin >> wifeIncome;
+
+				if (wifeIncome >= 0) {
+					cout << endl;
+					break;
+				}
+				else {
+					cout << "Invalid input, please try entering a dollar amount.";
+				}
+			} while (true);
+			
+			totalIncome = husbandIncome + wifeIncome;
+
+			do {
+				cout << "How many percent from your total income would you want allocated into savings?" << endl;
+				cin >> savingsPercent;
+
+				if (savingsPercent >= 0 && savingsPercent <= 100) {
+					
+					savingsRate = savingsPercent * 0.01;
+					savings = totalIncome * savingsRate;
+
+
+
+					cout << totalIncome<< endl;
+					cout << savingsPercent << endl;
+					cout << savingsRate << endl;
+					cout << savings << endl;
+					return 0;
+				}
+			} while (true);
+		
+
+			
+
+		
+		
+		
 		}
+
 	} while (true);
 
-	do {
-		cout << "How much was spent on gas?\n";
-		cin >> gasExpense;
-
-		if (gasExpense >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
-
-	do {
-		cout << "How much was spent on eating out?\n";
-		cin >> foodExpense;
-
-		if (foodExpense >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
-
-	do {
-		cout << "How much was spent on the mortgage?\n";
-		cin >> mortgageExpense;
-
-		if (mortgageExpense >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
-
-	do {
-		cout << "How much was spent on utilities?\n";
-		cin >> utilityExpense;
-
-		if (utilityExpense >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
-
-	do {
-		cout << "How much was spent on groceries?\n";
-		cin >> groceriesExpense;
-
-		if (groceriesExpense >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
-
-	do {
-		cout << "How much was spent on bills?\n";
-		cin >> billsExpense;
-
-		if (billsExpense >= 0) {
-			break;
-		}
-		else {
-			cout << "Invalid input please try to input a numerical value representing a dollar amount\n";
-		}
-	} while (true);
-
-	cin.ignore();
-
-	cout << "Do you put money in savings?\n";
-	getline(cin, save);
-
-	cout << "Do you put money aside for investing?\n";
-	getline(cin, invest);
-
-	cout << setprecision(2) << fixed;
-
-	totalIncome = husbandIncome + wifeIncome;
-
-	double gasPercent = (gasExpense / totalIncome) * 100;
-	double foodPercent = (foodExpense / totalIncome) * 100;
-	double mortgagePercent = (mortgageExpense / totalIncome) * 100;
-	double utilityPercent = (utilityExpense / totalIncome) * 100;
-	double groceriesPercent = (groceriesExpense / totalIncome) * 100;
-	double billsPercent = (billsExpense / totalIncome) * 100;
-
-	if (save == "yes" || save == "Yes" || save == "Y" || save == "y") {
-		cout << "How much did you set aside into savings?\n";
-		cin >> savings;
-	}
-
-	else if (save == "no" || save == "No" || save == "n" || save == "N") {
-		cout << endl;
-		savings = 0;
-	}
-
-	if (invest == "yes" || invest == "Yes" || invest == "Y" || invest == "y") {
-		cout << "How much was set aside for investments?\n";
-		cin >> investments;
-	}
-
-	else if (invest == "no" || invest == "No" || invest == "n" || invest == "N") {
-
-		cout << endl;
-		investments = 0;
-	}
 
 
-	totalIncome = calculateIncome(husbandIncome, wifeIncome);
-	totalExpense = calculateExpense(gasExpense, foodExpense, mortgageExpense, utilityExpense, groceriesExpense, billsExpense);
-	piggyBank = savings + investments;
-	piggyBankPercent = (piggyBank / totalIncome) * 100;
-	savingsPercent = (savings / totalIncome) * 100;
-	investmentPercent = (investments / totalIncome) * 100;
-	moneyRemaining = totalIncome - totalExpense - piggyBank;
 
-	cout << endl;
-
-	if (totalExpense > totalIncome) {
-		cout << "How the fuck are you going to spend more than you make???\n";
-	}
-
-	cout << endl;
-
-	cout << "Your total household income is $" << totalIncome << endl;
-	cout << "Your total expense is $" << totalExpense << endl;
-	cout << gasPercent << "% of the income was spent on gas." << endl;
-	cout << foodPercent << "% of the income was spent on eating out." << endl;
-	cout << mortgagePercent << "% of the income was spent on the mortgage." << endl;
-	cout << utilityPercent << "% of the income was spent on utilities." << endl;
-	cout << groceriesPercent << "% of the income was spent on groceries." << endl;
-	cout << billsPercent << "% of the income was spent on bills." << endl;
-	cout << endl;
-	cout << "Your total amount set aside for savings and investment is $" << piggyBank << endl;
-	cout << "This accounts for " << piggyBankPercent << "% of the total household income." << endl;
-	cout << "Your remaining cash available is $" << moneyRemaining;
-
-	cout << endl;
 	return 0;
 }
 
